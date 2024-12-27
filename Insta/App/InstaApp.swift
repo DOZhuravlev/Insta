3d2288b3475ac4b0c91da79dcc8edfe99187e44f
Zhuravlev Dmitry <qwerty-91@list.ru>
2024-12-27 19:22:29 +0500

    Initial Commit

---
+++ Insta/App/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by Zhuravlev Dmitry on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
